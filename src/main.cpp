f75733dc0aee740c0cb2b61b031978ac7228edbb
Ned Ely <S4172366@student.rmit.edu.au>
2026-03-30 13:48:48 +1100

    STUPID STUPID STUPID. commiting to hard coding the tetris path for now, added a macpath and windows path for easier switching between my computers. This is silly, not even really worth commiting and certainly wont be in the final build (if thats even worth saying)

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@
 
 int main()
 {
-    GB_MemoryBus::loadRom("C:/Users/Ned/Downloads/Tetris (World) (Rev 1)/Tetris (World) (Rev 1).gb");
+    // fuck it dude i dont give a shit this is stupid as fuck
+    // ill implement actual file input + commandline args, but for rn, this is fine
+    std::string macPath = "/Users/neddo/Downloads/Tetris (World) (Rev 1)/Tetris (World) (Rev 1).gb";
+    std::string windowsPath = "C:/Users/Ned/Downloads/Tetris (World) (Rev 1)/Tetris (World) (Rev 1).gb";
+    GB_MemoryBus::loadRom(macPath);
     Emulator emulator;
     for (;;)
     {
