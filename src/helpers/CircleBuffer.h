c9274f3aa97d4b08b6a95f0e4fbab0a0d64b7cb7
Ned Ely <nednedely@gmail.com>
2026-04-08 03:46:14 +1000

    made circlebuffer header only (because apparently template classes have to be! silly me) and fixed an off by one error with indexing as a result of poorly handled insert() logic

--- src/helpers/CircleBuffer.h
+++ src/helpers/CircleBuffer.h
@@ -11,16 +11,37 @@ class CircleBuffer
 {
     private:
         T* buffer;
+        // total internal size
         size_t bufferSize;
+        // length of filled buffer
+        size_t bufferLength = 0;
         // pointers to front and back of our buffer
         size_t startPointer = 0;
         size_t endPointer = 0;
     public:
-        CircleBuffer(size_t size);
-        void insert(T element);
-        T at(size_t index);
+        CircleBuffer(const size_t size) : bufferSize(size)
+        {
+            buffer = new T[bufferSize];
+        }
+        void insert(T element)
+        {
+            if (endPointer == bufferSize)
+            {
+                endPointer = 0;
+                startPointer++;
+            }
+            buffer[endPointer++] = element;
+            if (bufferLength < bufferSize) bufferLength++;
+        }
+        T at(size_t index)
+        {
+            // get the fixed memory index rather than the relative circular buffer index
+            // does this comment make sense? its 3am! who gaf!!
+            size_t fixedIndex = (startPointer + index) % bufferLength;
+            return buffer[fixedIndex];
+        }
+        size_t length() const { return bufferLength; }
 
 };
-
 
 #endif //GAMEBOY2_CIRCLEBUFFER_H
